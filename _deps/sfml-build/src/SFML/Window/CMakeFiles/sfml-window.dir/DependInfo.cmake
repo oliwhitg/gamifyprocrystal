2a1b5e524b8cc854240b25feaf845703b081592c
oliwhitg <48291306+oliwhitg@users.noreply.github.com>
2020-11-12 13:29:07 +0000

    Add files via upload

---
+++ _deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Clipboard.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Clipboard.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Context.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Context.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Cursor.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Cursor.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/GlContext.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/GlContext.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/GlResource.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Joystick.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Joystick.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/JoystickManager.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/JoystickManager.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Keyboard.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Keyboard.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Mouse.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Mouse.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Sensor.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Sensor.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/SensorManager.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/SensorManager.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Touch.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Touch.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/ClipboardImpl.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/ClipboardImpl.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/CursorImpl.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/CursorImpl.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/Display.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/Display.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/GlxContext.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/GlxContext.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/GlxExtensions.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/GlxExtensions.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/InputImpl.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/InputImpl.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/JoystickImpl.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/JoystickImpl.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/SensorImpl.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/SensorImpl.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/VideoModeImpl.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/VideoModeImpl.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Unix/WindowImplX11.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/WindowImplX11.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/VideoMode.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/VideoMode.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/Window.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Window.cpp.o"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-src/src/SFML/Window/WindowImpl.cpp" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/WindowImpl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_WINDOW_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/sfml-src/include"
+  "_deps/sfml-src/src"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-build/lib/libsfml-window.so" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/lib/libsfml-window.so.2.5.1"
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-build/lib/libsfml-window.so.2.5" "/home/oli/CLionProjects/buildgame/_deps/sfml-build/lib/libsfml-window.so.2.5.1"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/oli/CLionProjects/buildgame/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
