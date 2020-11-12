76c82bdae11a59716a391473c8fcb757aaf1948b
oliwhitg <48291306+oliwhitg@users.noreply.github.com>
2020-11-12 14:09:17 +0000

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/buildgame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buildgame.dir/main.cpp.o"
+  "buildgame"
+  "buildgame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buildgame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
